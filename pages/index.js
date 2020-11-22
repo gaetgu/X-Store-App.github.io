99f799a259bf2fa0164fd78526b94f0689924c31
agomila96@gmail.com <aitor2008>
2020-11-22 20:59:37 +0100

    Blank homescreen

--- pages/index.js
+++ pages/index.js
@@ -17,39 +17,7 @@ function Home() {
 			</Head>
 			<Navbar/>
 			<HomeLayout>
-				<div className="container">
-					<div className="horizontal">
-						<div className="image">
-							<img alt="XStore Logo" src={Logo}/>
-						</div>
-						<div className="right-text"> 
-							<Title>Made for every person in the world</Title>
-							<p>XStore is an open source, free and cross-platform app store</p>
-							<a href="/docs/faq">Any doubt? Check the FAQ</a>
-							<a target="_blank" href="https://github.com/X-Store-App/client/releases">
-								<Button>XSTORE DESKTOP</Button>
-							</a>
-							
-							<Link href="/browser" >
-								<SecondButton>Use browser version</SecondButton>
-							</Link>
-						</div>
-					</div>
-					<div className="footer">
-						<div>
-							<a title="GitHub Organization" target="_blank" href="https://github.com/X-Store-App">
-								<img alt="GitHub Organization" id="github_link" src="github_mark.png"/>
-							</a>
-							<a title="Discord Server" target="_blank" href="https://discord.com/invite/ByDn5PX">
-								<img alt="Discord Server" id="discord_link" src="discord.svg"/>
-							</a>
-						</div>
-						<div>
-							<p>Available for Windows, Linux, Android and any device running the Web.</p>
-						</div>
-						<Footer/>
-					</div>
-				</div>
+				
 			</HomeLayout>
 		</div>
 	)
