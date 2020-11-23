d5495b73923187efd1e7585c1a40eefc39199de1
agomila96@gmail.com <aitor2008>
2020-11-23 19:52:10 +0100

    Use functional component

--- pages/download.js
+++ pages/download.js
@@ -1,13 +1,11 @@
 import { useState, useEffect, useContext } from 'react'
 
-class Download {
-	render() {
-		return (
-			<div style={{height: '100%'}}>
-				
-			</div>
-		)
-	}
+function Download() {
+	return (
+		<div style={{height: '100%'}}>
+			
+		</div>
+	)
 }
 
 export default Download
