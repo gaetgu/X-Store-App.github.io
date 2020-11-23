6e176a99d6da76b433c0790dfab02c34f5352a9f
agomila96@gmail.com <aitor2008>
2020-11-23 19:53:11 +0100

    Add navbar

--- pages/download.js
+++ pages/download.js
@@ -1,9 +1,20 @@
-import { useState, useEffect, useContext } from 'react'
+import Navbar from '../components/navbar.jsx'
+import HomeLayout from '../components/home.layout.jsx'
+import Title from '../components/title.jsx'
+import Button from '../components/button.jsx'
+import Head from 'next/head'
+import Link from 'next/link'
 
 function Download() {
 	return (
-		<div style={{height: '100%'}}>
-			
+		<div>
+			<Head>
+				<title>Download</title>
+			</Head>
+			<Navbar/>
+			<HomeLayout>
+
+			</HomeLayout>
 		</div>
 	)
 }
